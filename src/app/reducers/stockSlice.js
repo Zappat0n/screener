a0243dd75293a1bfad961b54a8f7f106e34781db
Zappat0n <albarros1973@gmail.com>
2021-04-06 17:44:35 +0200

    Implement stockSlice

--- src/app/reducers/stockSlice.js
+++ src/app/reducers/stockSlice.js
@@ -1,15 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getQuote } from '../api/queries';
 
 const initialState = {
-
-}
+  AAPL: { name: 'Apple Inc.', price: '' },
+};
 
 export const stockSlice = createSlice({
   name: 'stocks',
   initialState,
   reducers: {
-
+    updateStocks: (state, action) => {
+      action.payload.forEach((stock) => {
+        // eslint-disable-next-line no-param-reassign
+        state[stock.symbol] = stock;
+      });
+    },
   },
 });
+
+export const refreshStocks = createAsyncThunk(
+  'stocks/updateStocks',
+  async (ticker) => {
+    const response = await getQuote(ticker);
+    stockSlice.actions.updateStocks(response);
+    return response;
+  },
+);
+
+export const { update, updateStocks } = stockSlice.actions;
 
 export default stockSlice.reducer;
