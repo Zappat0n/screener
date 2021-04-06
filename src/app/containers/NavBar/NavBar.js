5102e67787463ae7afced9e4c4bf0cfe5eff4dc7
Zappat0n <albarros1973@gmail.com>
2021-04-06 17:43:30 +0200

    Add update Stock button to NavBar

--- src/app/containers/NavBar/NavBar.js
+++ src/app/containers/NavBar/NavBar.js
@@ -1,21 +1,38 @@
-import { unwrapResult } from '@reduxjs/toolkit'
-import { useDispatch } from "react-redux";
-import { refreshIndexes, updateIndexes } from '../../reducers/indexSlice'
+import { unwrapResult } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
+import indexComponents from '../../data/components';
+import { refreshIndexes, updateIndexes } from '../../reducers/indexSlice';
+import { refreshStocks, updateStocks } from '../../reducers/stockSlice';
 
 const NavBar = () => {
   const dispatch = useDispatch();
-
+  const indexes = useSelector((state) => state.indexes);
+  console.log(Object.keys(indexes));
 
   return (
     <>
-      <button type="button" onClick={() => dispatch(refreshIndexes("index"))
-        .then(unwrapResult)
-        .then(originalPromiseResult => {
-          dispatch(updateIndexes(originalPromiseResult));
-        })
-      }>Refresh</button>
+      <button
+        type="button"
+        onClick={() => dispatch(refreshIndexes(Object.keys(indexes).join(',')))
+          .then(unwrapResult)
+          .then((originalPromiseResult) => {
+            dispatch(updateIndexes(originalPromiseResult));
+          })}
+      >
+        Refresh Indexes
+      </button>
+      <button
+        type="button"
+        onClick={() => dispatch(refreshStocks(indexComponents['^GSPC'].join(',')))
+          .then(unwrapResult)
+          .then((originalPromiseResult) => {
+            dispatch(updateStocks(originalPromiseResult));
+          })}
+      >
+        Refresh Stocks
+      </button>
     </>
   );
-}
+};
 
 export default NavBar;
