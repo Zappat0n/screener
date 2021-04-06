ddb3f7c57c6d231335654945e7348e1be49e5d66
Zappat0n <albarros1973@gmail.com>
2021-04-06 17:42:57 +0200

    Create Stock component

---
+++ src/app/containers/Stock/Stock.js
@@ -0,0 +1,27 @@
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Index = (props) => {
+  const { ticker } = props;
+  const selectName = useSelector((state) => state.stocks[ticker].name);
+  const selectPrice = useSelector((state) => state.stocks[ticker].price);
+  const selectChange = useSelector((state) => state.stocks[ticker].changesPercentage);
+
+  return (
+    <h4>
+      {selectName}
+      {' '}
+      :
+      {selectPrice}
+      {' '}
+      {selectChange}
+      %
+    </h4>
+  );
+};
+
+Index.propTypes = {
+  ticker: PropTypes.string.isRequired,
+};
+
+export default Index;
