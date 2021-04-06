c8bbe909d4138316d80ee4004c42d911937a81e9
Zappat0n <albarros1973@gmail.com>
2021-04-06 17:43:50 +0200

    Create MainContainer for stocks

--- src/app/containers/MainContainer/MainContainer.js
+++ src/app/containers/MainContainer/MainContainer.js
@@ -1,11 +1,23 @@
-import Indexes from "../Indexes/Indexes"
+import { useSelector } from 'react-redux';
+import Stock from '../Stock/Stock';
 
 const MainContainer = () => {
+  const components = useSelector((state) => state.stocks);
+
+  const createStocks = () => {
+    if (components) {
+      return Object.keys(components).map(
+        (quote) => <Stock key={quote} ticker={quote} />,
+      );
+    }
+    return [];
+  };
+
   return (
     <>
-    <Indexes/>
+      {createStocks()}
     </>
   );
-}
+};
 
 export default MainContainer;
