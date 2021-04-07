1f6dd78b3078cf130e62983d303e622fc2f4fdda
Zappat0n <albarros1973@gmail.com>
2021-04-07 10:59:30 +0200

    Add more stocks to US Indexes

--- src/app/data/components.js
+++ src/app/data/components.js
@@ -1,6 +1,6 @@
 const indexComponents = {
-  '^GSPC': ['MSFT', 'AAPL', 'AMZN', 'FB', 'GOOGL', 'GOOG', 'JNJ', 'BRK.B', 'V', 'PG', 'JPM', 'HD', 'UNH', 'MA', 'NVDA'], // , 'VZ', 'NFLX', 'ADBE', 'DIS', 'INTC', 'PYPL', 'T', 'PFE', 'CSCO', 'TSLA'
-  '^NDX': ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'GOOGL', 'FB', 'TSLA', 'NVDA', 'PYPL', 'INTC'], // 'ASML', 'CMCSA', 'NFLX', 'ADBE', 'CSCO', 'AVGO', 'PEP', 'TXN', 'PDD', 'TMUS', 'COST', 'QCOM', 'AMGN', 'AMAT', 'SBUX', 'JD', 'CHTR', 'INTU', 'MU', 'AMD'
+  '^GSPC': ['MSFT', 'AAPL', 'AMZN', 'FB', 'GOOGL', 'GOOG', 'JNJ', 'V', 'PG', 'JPM', 'HD', 'UNH', 'MA', 'NVDA', 'VZ', 'NFLX', 'ADBE', 'DIS', 'INTC', 'PYPL', 'T', 'PFE', 'CSCO'],
+  '^NDX': ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'GOOGL', 'FB', 'TSLA', 'NVDA', 'PYPL', 'INTC', 'ASML', 'CMCSA', 'NFLX', 'ADBE', 'CSCO', 'AVGO', 'PEP', 'TXN', 'PDD', 'TMUS', 'COST', 'QCOM', 'AMGN', 'AMAT', 'SBUX', 'JD', 'CHTR', 'INTU', 'MU', 'AMD'],
   '^STOXX50E': ['MC.PA', 'ASML.AS', 'OR.PA', 'PRX.AS', 'VOW.DE', 'SAP.DE', 'LIN', 'SIE.DE', 'ABI.BR', 'SAN.PA'],
 };
 
