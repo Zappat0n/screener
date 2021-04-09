3b11b8b53dee5237b786976628d23191dcf446b5
Zappat0n <albarros1973@gmail.com>
2021-04-09 16:27:28 +0200

    Set up environment for testing

---
+++ src/app/test-utils.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createStore, combineReducers } from 'redux';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import indexReducer from './reducers/indexSlice';
+import stockReducer from './reducers/stockSlice';
+import profileReducer from './reducers/profileSlice';
+import filterReducer from './reducers/filterSlice';
+
+const rootReducer = combineReducers({
+  indexes: indexReducer, stocks: stockReducer, profile: profileReducer, filter: filterReducer,
+});
+
+const initialState = {
+  stocks: {
+    AAPL: {
+      price: 100,
+      companyName: 'Apple Inc',
+      changes: 1,
+      sector: 'Technology',
+    },
+  },
+};
+
+const renderComponent = (component) => render(
+  <Provider store={createStore(rootReducer, initialState)}>
+    <BrowserRouter>
+      {component}
+    </BrowserRouter>
+  </Provider>,
+);
+
+export * from '@testing-library/react';
+export { renderComponent };
