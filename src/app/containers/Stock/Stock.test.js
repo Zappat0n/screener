6e734358475337a6a7f205ed2e85d2c74f4e6919
Zappat0n <albarros1973@gmail.com>
2021-04-09 16:27:39 +0200

    Add tests for stock

--- src/app/containers/Stock/Stock.test.js
+++ src/app/containers/Stock/Stock.test.js
@@ -1,7 +1,20 @@
+import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import Stock from './Stock';
+import { renderComponent } from '../../test-utils';
 
-test('calculates percentage correctly', () => {
-  render(<Stock key="AAPL" ticker="AAPL" handleClick={null} />);
+renderComponent(
+  <table>
+    <tbody>
+      <Stock ticker="AAPL" />
+    </tbody>
+  </table>,
+);
+
+test('Renders Stock correctly', () => {
+  expect(screen.getByText('Apple Inc')).toBeInTheDocument();
+  expect(screen.getAllByText('100')[0]).toBeInTheDocument();
+  expect(screen.getByText('1%')).toBeInTheDocument();
+  expect(screen.getByText('Technology')).toBeInTheDocument();
 });
