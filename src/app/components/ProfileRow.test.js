8f102ec174db30a6f608c25538b4fbc9f379f441
Zappat0n <albarros1973@gmail.com>
2021-04-10 12:13:35 +0200

    Fix linter error

--- src/app/components/ProfileRow.test.js
+++ src/app/components/ProfileRow.test.js
@@ -2,15 +2,22 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 import ProfileRow from './ProfileRow';
-import { renderComponent } from '../test-utils';
+import { renderComponent, rendererForComponent } from '../test-utils';
 
 renderComponent(
   <ProfileRow title1="Name" data1="Me" title2="Sector" data2="Mine" />,
 );
 
-test('Renders Index correctly', () => {
+test('Renders ProfileRow correctly', () => {
   expect(screen.getByText('Name')).toBeInTheDocument();
   expect(screen.getByText('Me')).toBeInTheDocument();
   expect(screen.getByText('Sector')).toBeInTheDocument();
   expect(screen.getByText('Mine')).toBeInTheDocument();
+});
+
+test('ProfileRow Match SnapShot', () => {
+  const tree = rendererForComponent(
+    <ProfileRow title1="Name" data1="Me" title2="Sector" data2="Mine" />,
+  );
+  expect(tree).toMatchSnapshot();
 });
