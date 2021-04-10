3369044024af18d78c3f8dc13da731deec12df28
Zappat0n <albarros1973@gmail.com>
2021-04-10 12:30:23 +0200

    Add integration test for Stocks

--- src/app/containers/Indexes/Indexes.test.js
+++ src/app/containers/Indexes/Indexes.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import Indexes from './Indexes';
 import { renderComponent, rendererForComponent } from '../../test-utils';
 
@@ -20,13 +20,12 @@ test('Indexes Match SnapShot', () => {
   expect(tree).toMatchSnapshot();
 });
 
-/*
-test('Display Stocks', () => {
+test('Integration test for Stocks', () => {
   renderComponent(
     <Indexes />,
   );
 
-  fireEvent.click(screen.getByText('S&P 500'), { button: 1 });
-  expect(screen.findByText('Apple')).toBeInTheDocument();
+  expect(screen.getByText('S&P 500').closest('a')).toHaveAttribute('href', '/index/GSPC');
+  fireEvent.click(screen.getByText('S&P 500').closest('a'));
+  waitFor(() => expect(screen.findByText('Apple Inc')).toBeInTheDocument());
 });
-*/
